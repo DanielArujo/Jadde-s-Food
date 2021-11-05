09d2fcdb6612d40aabf575d20b3b7caf18a9d8ea
DanielAraujo <daniel.araujos22004@gmail.com>
2021-11-04 21:48:19 -0300

    hmmm

--- react/jaddes/src/pages/home/index.js
+++ react/jaddes/src/pages/home/index.js
@@ -8,11 +8,11 @@ export default function Home(){
         <Container>
             <div className='link'> <Link to= '/produtos' > <button> Add Produtos  </button></Link></div>
             <div className='link'> <Link to='/aprovar'>  <button> Aprovar Compras </button> </Link> </div>
-            <div className='link'> <Link to='./adicionar produto/index.html'> <button>  Adiocionar Produtos</button> </Link></div>
+            <div className='link'> <Link to='./adicionar'> <button>  Adiocionar Produtos</button> </Link></div>
             <div className='link'> <Link to='/login'> <button> Entrar-cliente </button> </Link> </div>
             <div className='link'> <Link to='/create'> <button> Criar Conta </button> </Link> </div>
             <div className='link'> <Link to='/esqueci'> <button> Esqueceu Senha </button> </Link> </div>
-            <div className='link'> <Link to='tela-inicial'> <button> Tela Inicial </button> </Link> </div>
+            <div className='link'> <Link to='/tela-inicial'> <button> Tela Inicial </button> </Link> </div>
             <div className='link'> <Link to='/lanches'> <button> Cardapio - Lanches </button> </Link> </div>
             <div className='link'> <Link to='/pedidos'> <button> Pedidos - Amd </button> </Link> </div>
         </Container>
